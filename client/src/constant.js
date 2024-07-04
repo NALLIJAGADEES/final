1db4c0653dae2b20a7a286845e2768b464120263
NALLIJAGADEES <158473780+NALLIJAGADEES@users.noreply.github.com>
2024-07-04 20:13:40 +0530

    Add files via upload

---
+++ client/src/constant.js
@@ -0,0 +1,3 @@
+const server_url="https://new-1-15js.onrender.com";
+
+export { server_url}
---
+++ server/DataBase/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async () => {
+  console.log("a")
+  const url ="mongodb+srv://Nalli:Nalli123@cluster0.ehhmtay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  
+  try {
+    console.log("b")
+    await mongoose.connect(url);
+    console.log("db is connected");
+  } catch (error) {
+    console.error("error connecting to mongo", error);
+  }
+};
+
+export default connectDB;
